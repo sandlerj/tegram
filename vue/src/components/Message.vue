2f6c9109c6f2e13e739fa09dd89edc7416e57efd
Andrew Lamansky <andrewlamansky@gmail.com>
2021-12-16 12:07:14 -0500

    create message component and add to PostList

---
+++ vue/src/components/Message.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message" v-show="show" style="margin: 2rem">
+  <div class="message-header">
+    <p>{{ header }}</p>
+    <font-awesome-icon icon="times" @click="closeMessage"/>
+  </div>
+  <div class="message-body">
+      <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "message",
+    props: ["header"],
+    data() {
+        return {
+            show: true
+        }
+    },
+    methods: {
+        closeMessage() {
+            this.show = false;
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
